Correcting performance calculation

mp/mp.py
@@ -24,6 +24,7 @@ Y = linspace(ymin,ymax,ny) # pixel co-ordinates
 
 # main loops
 if __name__ == '__main__':
+    start = time.perf_counter()
     p = Pool()
     Z = [complex(x,y) for y in Y for x in X]
     N = p.map(mandelbrot,Z)
@@ -35,4 +36,5 @@ if __name__ == '__main__':
     data = im.fromarray((N*255).astype(np.uint16))
     data.save('mandelbrot.png') 
     finish = time.perf_counter()
-    print(f'Finished running after seconds:', finish)
+    perf = finish - start
+    print(f'Finished running after seconds:', perf)

normal/normal.py
@@ -26,6 +26,8 @@ def mandelbrot(z): # computation for one pixel
     z = z*z + c
   return maxiter
 
+start = time.perf_counter()
+
 X = linspace(xmin,xmax,nx) # lists of x and y
 Y = linspace(ymin,ymax,ny) # pixel co-ordinates
 
@@ -36,9 +38,10 @@ for y in Y:
     z  = complex(x,y)
     N += [mandelbrot(z)]
 
-#N = reshape(N, (nx,ny)) # change to rectangu
-#data = im.fromarray((N*255).astype(np.uint16))
-#data.save('mandelbrot.png') 
+N = reshape(N, (nx,ny)) # change to rectangu
+data = im.fromarray((N*255).astype(np.uint16))
+data.save('mandelbrot.png') 
 
 finish = time.perf_counter()
-print(f'Finished running after seconds:', finish)
+perf = finish - start
+print(f'Finished running after seconds:', perf)